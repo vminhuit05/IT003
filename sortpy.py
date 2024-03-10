df343fbc240ce4a087037133a122d61143c20162
Nguyễn Văn Minh <161409681+vminhuit05@users.noreply.github.com>
2024-03-10 09:48:25 +0700

    Add files via upload

--- sortpy.py
+++ sortpy.py
@@ -1,18 +1,18 @@
-# import numpy as np
-# import pandas as pd
-# import time
-#
-# for i in range(1,11):
-#     f = open(f'number.inp{i}.txt','r')
-#     data = f.read().split(" ")
-#     # print(data)
-#     #print(type(data))
-#     my_integers = [int(x) for x in data]
-#     # print(my_integers)
-#     start_time = time.time()
-#     a = np.sort(my_integers)
-#     end_time = time.time()
-#
-#     det = end_time - start_time
-#     print(det*1000)
+import numpy as np
+import pandas as pd
+import time
+
+for i in range(1,11):
+    f = open(f'number.inp{i}.txt','r')
+    data = f.read().split(" ")
+    # print(data)
+    #print(type(data))
+    my_integers = [float(x) for x in data]
+    # print(my_integers)
+    start_time = time.time()
+    a = np.sort(my_integers)
+    end_time = time.time()
+
+    det = end_time - start_time
+    print(det*1000)
 
